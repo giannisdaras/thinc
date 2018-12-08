8ef49fa289904377ee082ed790c4e4edef2a0801
Giannis Daras <daras.giannhs@gmail.com>
2018-12-09 00:32:07 +0200

    Fix PEP8 to datasets loader

--- thinc/extra/datasets.py
+++ thinc/extra/datasets.py
@@ -1,13 +1,13 @@
 from __future__ import unicode_literals
-import random # pragma: no cover
-import io # pragma: no cover
-from collections import Counter # pragma: no cover
-import os.path # pragma: no cover
-import csv # pragma: no cover
+import random  # pragma: no cover
+import io  # pragma: no cover
+from collections import Counter  # pragma: no cover
+import os.path  # pragma: no cover
+import csv  # pragma: no cover
 import numpy
 import json
 
-from ._vendorized.keras_data_utils import get_file # pragma: no cover
+from ._vendorized.keras_data_utils import get_file  # pragma: no cover
 from ..neural.util import partition
 from ..neural.util import to_categorical
 from ..compat import basestring, Path
@@ -18,18 +18,19 @@ except NameError:
     unicode = str
 
 
-GITHUB = 'https://github.com/UniversalDependencies/' # pragma: no cover
+GITHUB = 'https://github.com/UniversalDependencies/'  # pragma: no cover
 ANCORA_1_4_ZIP = '{github}/{ancora}/archive/r1.4.zip'.format(
-    github=GITHUB, ancora='UD_Spanish-AnCora') # pragma: no cover
+    github=GITHUB, ancora='UD_Spanish-AnCora')  # pragma: no cover
 EWTB_1_4_ZIP = '{github}/{ewtb}/archive/r1.4.zip'.format(
-    github=GITHUB, ewtb='UD_English') # pragma: no cover
+    github=GITHUB, ewtb='UD_English')  # pragma: no cover
 
 SNLI_URL = 'http://nlp.stanford.edu/projects/snli/snli_1.0.zip'
-QUORA_QUESTIONS_URL = 'http://qim.ec.quoracdn.net/quora_duplicate_questions.tsv'
+QUORA_QUESTIONS_URL = \
+    'http://qim.ec.quoracdn.net/quora_duplicate_questions.tsv'
 IMDB_URL = 'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
 
 
-def ancora_pos_tags(encode_words=False): # pragma: no cover
+def ancora_pos_tags(encode_words=False):  # pragma: no cover
     data_dir = get_file('UD_Spanish-AnCora-r1.4', ANCORA_1_4_ZIP,
                         unzip=True)
     train_loc = os.path.join(data_dir, 'es_ancora-ud-train.conllu')
@@ -37,7 +38,7 @@ def ancora_pos_tags(encode_words=False): # pragma: no cover
     return ud_pos_tags(train_loc, dev_loc, encode_words=encode_words)
 
 
-def ewtb_pos_tags(encode_tags=False, encode_words=False): # pragma: no cover
+def ewtb_pos_tags(encode_tags=False, encode_words=False):  # pragma: no cover
     data_dir = get_file('UD_English-r1.4', EWTB_1_4_ZIP, unzip=True)
     train_loc = os.path.join(data_dir, 'en-ud-train.conllu')
     dev_loc = os.path.join(data_dir, 'en-ud-dev.conllu')
@@ -45,7 +46,8 @@ def ewtb_pos_tags(encode_tags=False, encode_words=False): # pragma: no cover
         encode_tags=encode_tags, encode_words=encode_words)
 
 
-def ud_pos_tags(train_loc, dev_loc, encode_tags=True, encode_words=True): # pragma: no cover
+# pragma no cover
+def ud_pos_tags(train_loc, dev_loc, encode_tags=True, encode_words=True):
     train_sents = list(read_conll(train_loc))
     dev_sents = list(read_conll(dev_loc))
     tagmap = {}
@@ -61,7 +63,7 @@ def ud_pos_tags(train_loc, dev_loc, encode_tags=True, encode_words=True): # prag
     def _encode(sents):
         X = []
         y = []
-        for words, tags  in sents:
+        for words, tags in sents:
             if encode_words:
                 X.append(
                     numpy.asarray(
@@ -118,7 +120,7 @@ def read_imdb(data_dir, limit=0):
     return examples
 
 
-def read_conll(loc): # pragma: no cover
+def read_conll(loc):  # pragma: no cover
     n = 0
     with io.open(loc, encoding='utf8') as file_:
         sent_strs = file_.read().strip().split('\n\n')
@@ -147,7 +149,7 @@ def read_csv(csv_loc, label_col=0, text_col=-1):
             yield text, label_str
 
 
-def mnist(): # pragma: no cover
+def mnist():  # pragma: no cover
     from ._vendorized.keras_datasets import load_mnist
 
     # the data, shuffled and split between tran and test sets
@@ -169,7 +171,7 @@ def mnist(): # pragma: no cover
     return train_data, heldout_data, test_data
 
 
-def reuters(): # pragma: no cover
+def reuters():  # pragma: no cover
     from ._vendorized.keras_datasets import load_reuters
     (X_train, y_train), (X_test, y_test) = load_reuters()
     return (X_train, y_train), (X_test, y_test)
@@ -200,6 +202,8 @@ def quora_questions(loc=None):
 
 THREE_LABELS = {'entailment': 2, 'contradiction': 1, 'neutral': 0}
 TWO_LABELS = {'entailment': 1, 'contradiction': 0, 'neutral': 0}
+
+
 def snli(loc=None, ternary=False):
     label_scheme = THREE_LABELS if ternary else TWO_LABELS
     if loc is None:
@@ -232,11 +236,12 @@ def read_snli(loc, label_scheme):
             label = eg['gold_label']
             if label == '-':
                 continue
-            rows.append(((eg['sentence1'], eg['sentence2']), label_scheme[label]))
+            rows.append(((eg['sentence1'], eg['sentence2']),
+                        label_scheme[label]))
     return rows
 
 
-def get_word_index(path='reuters_word_index.pkl'): # pragma: no cover
+def get_word_index(path='reuters_word_index.pkl'):  # pragma: no cover
     path = get_file(path, origin='https://s3.amazonaws.com/text-datasets/reuters_word_index.pkl')
     f = open(path, 'rb')
 
