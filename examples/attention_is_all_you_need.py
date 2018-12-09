fe47f4c5ef6ea8c7b34a2d9f82fff210d74eb60b
Giannis Daras <daras.giannhs@gmail.com>
2018-12-09 22:44:52 +0200

    Get train,test and dev data

--- examples/attention_is_all_you_need.py
+++ examples/attention_is_all_you_need.py
@@ -1,14 +1,17 @@
 from thinc.neural.ops import NumpyOps, Ops
 import plac
+from thinc.extra.datasets import get_iwslt
 
 
 @plac.annotations(
-    heads=("number of heads of the multiheaded attention", "positional",
-           None, int),
+    heads=("number of heads of the multiheaded attention", "option"),
     dropout=("model dropout", "option")
 )
-def main(heads, dropout=0.1):
-    print(dropout)
+def main(heads=6, dropout=0.1):
+    train, dev, test = get_iwslt()
+    X_train, Y_train = zip(*train)
+    X_dev, Y_dev = zip(*dev)
+    X_test, Y_test = zip(*test)
 
 
 if __name__ == '__main__':
