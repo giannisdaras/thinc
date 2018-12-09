2ee6ea66a72bae71be4f60f126ad10cb82a78861
Giannis Daras <daras.giannhs@gmail.com>
2018-12-10 00:16:12 +0200

    Initialize begin training in attention is all you need example

--- examples/attention_is_all_you_need.py
+++ examples/attention_is_all_you_need.py
@@ -1,17 +1,41 @@
-from thinc.neural.ops import NumpyOps, Ops
+from thinc.neural.ops import NumpyOps, CupyOps, Ops
+from thinc.neural.optimizers import initNoAm
+from thinc.v2v import Model
 import plac
+import spacy
 from thinc.extra.datasets import get_iwslt
 
+
+class ModelException(Exception):
+    pass
+
+class BatchesException(Exception):
+    pass
 
 @plac.annotations(
     heads=("number of heads of the multiheaded attention", "option"),
     dropout=("model dropout", "option")
 )
 def main(heads=6, dropout=0.1):
+    if (CupyOps.xp != None):
+        Model.ops = CupyOps()
+        Model.Ops = CupyOps
+        print('Training on GPU')
+    else:
+        print('Training on CPU')
     train, dev, test = get_iwslt()
-    X_train, Y_train = zip(*train)
-    X_dev, Y_dev = zip(*dev)
-    X_test, Y_test = zip(*test)
+    train_X, train_Y = zip(*train)
+    dev_X, dev_Y = zip(*dev)
+    test_X, test_Y = zip(*test)
+    tokenizer = lambda x: spacy.load('en_core_web_sm').tokenizer(x)
+    vectorizer = spacy.load('en_vectors_web_lg')
+    raise BatchesException('Batchify not implemented yet.')
+    raise ModelException('Model not composed yet.')
+    with model.begin_training(train_X, train_Y, optimizer=initNoAm(input_size)) \
+            as (trainer, optimizer):
+            raise NotImplementedError
+
+
 
 
 if __name__ == '__main__':
