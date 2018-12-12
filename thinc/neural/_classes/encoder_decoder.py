2b866d9b45d04ea4b138fabd8aa648dc7f8fe600
Giannis Daras <daras.giannhs@gmail.com>
2018-12-12 18:23:18 +0200

    Add multiheaded attention class

--- thinc/neural/_classes/encoder_decoder.py
+++ thinc/neural/_classes/encoder_decoder.py
@@ -92,3 +92,57 @@ class DecoderLayer:
         y, other_attn_back = self.residuals[1].begin_update(y, lambda x: self.other_attention(y, x, x_mask))
         y, ffd_back = self.ffd.begin_update(y)
         return y, [ffd_back, other_attn_back, slf_attn_back]
+
+class MultiHeadedAttention(Model):
+    ''' This class implements multiheaded attention. It can be used for self
+    attention or outer attention, depending on our needs. There is no left
+    and right context width. We attend to the whole sentence and we take
+    care of the masks to adjust appropriately. There are no actual different
+    weight matrices for each head, but a bigger weight matrix for all heads.
+    Going to bigger dimensions is the key to get the multiple heads.
+    For the time being; key, query and value matrices are supposed to have the
+    same length.
+    '''
+    def __init__(self, nI, heads=6):
+        self.heads = heads
+        self.nI = nI  # model size: the length of the embeddings
+        self.nK = nI // heads
+        self.linears = [LinearModel(nI, nI) for i in range(4)]
+
+    def begin_update(X, y, mask):
+        nB = X.shape[0]
+        query, query_backprop = self.linears[0].begin_update(X)
+        query = query.reshape(nB, -1, self.heads, self.nK)
+        key, key_backprop = self.linears[1].begin_update(y)
+        key = key.reshape(nB, -1, self.heads, self.nK)
+        value, value_backprop = self.linears[2].begin_update(y)
+        value.reshape(nB, -1, self.heads, self.nK)
+        X = attn(query, key, value, mask=mask)
+        X = X.reshape(1, 2).reshape(nB, -1, self.heads * self.nK)
+        X, out_backprop = self.linears[-1].begin_update(X)
+        return X, [out_backprop, value_backprop, key_backprop, query_backprop]
+
+    def attn(self, query, key, value, mask=None):
+        ''' Compute attention based on query, key, value
+        Expected data size: nB x -1 x nT x nK
+        '''
+        nB = query.shape[0]
+        # number of tokens in each sentence
+        nT = query.shape[2]
+        # key length
+        nK = query.shape[3]
+        scores = self.ops.xp.matmul(query, key.transpose(0, 1, 3, 2)) \
+            / math.sqrt(self.nI)
+        # penalize masked tokens
+        scores[self.ops.xp.where(mask == 0)] = 1e-9
+        # TODO: fix this!
+        # this could be done much faster if softmax was supported for >= 3d.
+        for batch in range(nB):
+            scores[batch, :, :, :] = self.ops.softmax(scores[batch, :, :])
+        ''' Now the dimensions of scores are:
+        nB x nT x nT
+        We multiply by values which is nB x nT x nK, so the result is:
+        nB x nT x nK
+        '''
+        real_scores = self.ops.xp.matmul(scores, value)
+        return real_scores
