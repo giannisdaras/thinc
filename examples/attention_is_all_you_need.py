01e8b510245dc7a5f8db3be2c22c02c13a0d39b0
Giannis Daras <daras.giannhs@gmail.com>
2018-12-12 14:21:07 +0200

    Get positional encodings for the max sentence in batch

--- examples/attention_is_all_you_need.py
+++ examples/attention_is_all_you_need.py
@@ -11,13 +11,21 @@ from spacy.lang.en import English
 import pdb
 
 
+class Batch:
+    def __init__(self, X, y, X_mask, y_mask):
+        self.X = X
+        self.y = y
+        self.X_mask = X_mask
+        self.y_mask
+        self.batch_size = X.shape[0]
+
+
 def vectorize(batch, vectorizer):
     ''' Vectorize a batch of tokens with a vectorizer nlp object
     A batch (non-vectorized) contains some sentences (s) and some tokens (t).
     A vectorized batch contains some sentences(s) and the d-dimensional vectorized
     version of the tokens (d).
     '''
-    # pdb.set_trace()
     nB = len(batch)
     nT = len(batch[0])
     nD = len(vectorizer.vocab['test'].vector)
@@ -37,9 +45,9 @@ def main(heads=6, dropout=0.1):
     if (CupyOps.xp != None):
         Model.ops = CupyOps()
         Model.Ops = CupyOps
-        print('Training on GPU')
+        print('Running on GPU')
     else:
-        print('Training on CPU')
+        print('Running on CPU')
     train, dev, test = get_iwslt()
     train_X, train_Y = zip(*train)
     dev_X, dev_Y = zip(*dev)
@@ -56,7 +64,15 @@ def main(heads=6, dropout=0.1):
     with model.begin_training(train_X, train_Y, batch_size=2, nb_epoch=1) as (trainer, optimizer):
         for X, y, X_mask, y_mask in trainer.batch_mask(train_X, train_Y):
             X, y = vectorize(X, vectorizer), vectorize(y, vectorizer)
-            pass
+            batch_size = X.shape[0]
+            max_sentence_in_batch = X.shape[1]
+            model_size = X.shape[2]
+            X_positions = model.ops.position_encode(max_sentence_in_batch, model_size)
+            print(X_positions.shape)
+            print(X.shape)
+            break
+            batch = Batch(X, y, X_mask, y_mask)
+            yh, backprop = model.begin_update(batch, drop=trainer.dropout)
 
 
 if __name__ == '__main__':
