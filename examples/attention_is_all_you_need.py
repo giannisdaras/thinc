946a46258a5ae912a261fed358d9ccbc7f11c5b3
Giannis Daras <daras.giannhs@gmail.com>
2019-03-15 00:09:21 +0200

    Comment out all the main part and keep only dev code

--- examples/attention_is_all_you_need.py
+++ examples/attention_is_all_you_need.py
@@ -43,53 +43,68 @@ def main(heads=6, dropout=0.1, stack=6, batch_size=2):
         print('Running on GPU')
     else:
         print('Running on CPU')
-#
-    train, dev, test = get_iwslt()
-    train_X, train_Y = zip(*train)
-    dev_X, dev_Y = zip(*dev)
-    test_X, test_Y = zip(*test)
+
     model = EncoderDecoder()
-    X_positions = Model.ops.position_encode(50, MODEL_SIZE)
-    print('Position encodings computed successfully')
-    ''' Read dataset '''
-    nlp_en = spacy.load('en_core_web_md')
-    nlp_de = spacy.load('de_core_news_md')
-    eos_vector = Model.ops.xp.random.rand(MODEL_SIZE)
-    bos_vector = Model.ops.xp.random.rand(MODEL_SIZE)
-    nlp_de.vocab.set_vector('<eos>', eos_vector)
-    nlp_de.vocab.set_vector('<bos>', bos_vector)
-    en_tokenizer = English().Defaults.create_tokenizer(nlp_en)
-    de_tokenizer = German().Defaults.create_tokenizer(nlp_de)
-    train_X, dev_X, test_X = spacy_tokenize(en_tokenizer, train_X[:20],
-                                            dev_X[:10], test_X[:10])
-    train_Y, dev_Y, test_Y = spacy_tokenize(de_tokenizer, train_Y[:20],
-                                            dev_Y[:10], test_Y[:10])
-    en_embeddings = StaticVectors('en_core_web_md', MODEL_SIZE, column=0)
-    de_embeddings = StaticVectors('de_core_news_md', MODEL_SIZE, column=0)
-    en_word2indx, en_indx2word = numericalize_vocab(nlp_en)
-    de_word2indx, de_indx2word = numericalize_vocab(nlp_de)
-    with model.begin_training(train_X, train_Y, batch_size=2, nb_epoch=1) as \
-            (trainer, optimizer):
-        ''' add beginning and ending of sentence marks '''
-        train_Y = add_eos_bos(train_Y)
-        for X, y, X_mask, y_mask in trainer.batch_mask(train_X, train_Y):
-            ''' numericalize text '''
-            for indx, _x in enumerate(X):
-                X[indx] = numericalize(en_word2indx, _x)
-            for indx, _y in enumerate(y):
-                y[indx] = numericalize(de_word2indx, _y)
-            y_num = y
-            ''' get text embeddings '''
-            X = en_embeddings(Model.ops.asarray(X))
-            y = de_embeddings(Model.ops.asarray(y))
-            sentence_size = X.shape[1]
-            ''' add position encodings '''
-            X = X + X_positions[:sentence_size]
-            y = y + X_positions[:sentence_size]
-            batch = Batch(X, y, X_mask, y_mask)
-            yh, backprop = model.begin_update(batch, drop=trainer.dropout)
-            x0, x1, x2 = yh.shape
-            backprop(Model.ops.xp.random.rand(x0, x1, x2))
+    X = Model.ops.xp.random.rand(2, 17, 300)
+    y = Model.ops.xp.random.rand(2, 17, 300)
+    batch = Batch(X, y, None, None)
+    yh, backprop = model.begin_update(batch)
+    _1, _2, _3 = yh.shape
+    backprop(Model.ops.xp.random.rand(_1, _2, _3, dtype=Model.ops.xp.float32))
+    # train, dev, test = get_iwslt()
+    # train_X, train_Y = zip(*train)
+    # dev_X, dev_Y = zip(*dev)
+    # test_X, test_Y = zip(*test)
+    # model = EncoderDecoder()
+    # X_positions = Model.ops.position_encode(50, MODEL_SIZE)
+    # print('Position encodings computed successfully')
+    # ''' Read dataset '''
+    # nlp_en = spacy.load('en_core_web_md')
+    # nlp_de = spacy.load('de_core_news_md')
+    # print('Models loaded')
+    # eos_vector = Model.ops.xp.random.rand(MODEL_SIZE)
+    # bos_vector = Model.ops.xp.random.rand(MODEL_SIZE)
+    # nlp_de.vocab.set_vector('<eos>', eos_vector)
+    # nlp_de.vocab.set_vector('<bos>', bos_vector)
+    # en_tokenizer = English().Defaults.create_tokenizer(nlp_en)
+    # de_tokenizer = German().Defaults.create_tokenizer(nlp_de)
+    # train_X, dev_X, test_X = spacy_tokenize(en_tokenizer, train_X[:20],
+    #                                         dev_X[:10], test_X[:10])
+    # train_Y, dev_Y, test_Y = spacy_tokenize(de_tokenizer, train_Y[:20],
+    #                                         dev_Y[:10], test_Y[:10])
+    # print('Text tokenized')
+    # en_embeddings = StaticVectors('en_core_web_md', MODEL_SIZE, column=0)
+    # de_embeddings = StaticVectors('de_core_news_md', MODEL_SIZE, column=0)
+    # print('Static vectors loaded')
+    # en_word2indx, en_indx2word = numericalize_vocab(nlp_en)
+    # de_word2indx, de_indx2word = numericalize_vocab(nlp_de)
+    # print('Vocab numericalized')
+    # with model.begin_training(train_X, train_Y, batch_size=2, nb_epoch=1) as \
+    #         (trainer, optimizer):
+    #     ''' add beginning and ending of sentence marks '''
+    #     train_Y = add_eos_bos(train_Y)
+    #     for X, y, X_mask, y_mask in trainer.batch_mask(train_X, train_Y):
+    #         ''' numericalize text '''
+    #         for indx, _x in enumerate(X):
+    #             X[indx] = numericalize(en_word2indx, _x)
+    #         for indx, _y in enumerate(y):
+    #             y[indx] = numericalize(de_word2indx, _y)
+    #         print('Text numericalized')
+    #         y_num = y
+    #         ''' get text embeddings '''
+    #         X = en_embeddings(Model.ops.asarray(X))
+    #         y = de_embeddings(Model.ops.asarray(y))
+    #         sentence_size = X.shape[1]
+    #         ''' add position encodings '''
+    #         X = X + X_positions[:sentence_size]
+    #         y = y + X_positions[:sentence_size]
+    #         X = X.astype(Model.ops.xp.float32)
+    #         y = y.astype(Model.ops.xp.float32)
+    #         batch = Batch(X, y, X_mask, y_mask)
+    #         print('Forward pass begins')
+    #         yh, backprop = model.begin_update(batch, drop=trainer.dropout)
+    #         x0, x1, x2 = yh.shape
+    #         backprop(Model.ops.xp.random.rand(x0, x1, x2).astype(Model.ops.xp.float32))
 
 
 if __name__ == '__main__':
