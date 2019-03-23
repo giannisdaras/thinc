4b2d3c2449546edb6744e2ce6178aa19ef98076a
Giannis Daras <daras.giannhs@gmail.com>
2019-03-24 01:40:42 +0200

    Add function for progress tracking

--- examples/attention_is_all_you_need.py
+++ examples/attention_is_all_you_need.py
@@ -129,7 +129,7 @@ def spacy_tokenize(X_tokenizer, Y_tokenizer, X, Y, max_length=50):
     for x, y in zip(X, Y):
         xdoc = X_tokenizer(x)
         ydoc = Y_tokenizer(y)
-        if len(xdoc) < max_length and (len(ydoc) + 2) < max_length:
+        if len(xdoc) < MAX_LENGTH and (len(ydoc) + 2) < MAX_LENGTH:
             X_out.append([w.text for w in xdoc])
             Y_out.append([w.text for w in ydoc])
     return X_out, Y_out
@@ -159,12 +159,11 @@ def main(nH=6, dropout=0.1, nS=6, nB=15, nE=20, use_gpu=-1, lim=2000):
     if use_gpu != -1:
         # TODO: Make specific to different devices, e.g. 1 vs 0
         spacy.require_gpu()
-    max_length = 50
     train, dev, test = get_iwslt()
     train_X, train_Y = zip(*train)
     dev_X, dev_Y = zip(*dev)
     test_X, test_Y = zip(*test)
-    X_positions = Model.ops.position_encode(max_length, MODEL_SIZE)
+    X_positions = Model.ops.position_encode(MAX_LENGTH, MODEL_SIZE)
     print('Position encodings computed successfully')
     ''' Read dataset '''
     nlp_en = spacy.load('en_core_web_sm')
@@ -177,9 +176,14 @@ def main(nH=6, dropout=0.1, nS=6, nB=15, nE=20, use_gpu=-1, lim=2000):
     # nlp_de.vocab.set_vector('<bos>', bos_vector)
     en_tokenizer = English().Defaults.create_tokenizer(nlp_en)
     de_tokenizer = German().Defaults.create_tokenizer(nlp_de)
-    train_X, train_Y = spacy_tokenize(en_tokenizer, de_tokenizer, train_X[:lim], train_Y[:lim], max_length)
-    dev_X, dev_Y = spacy_tokenize(en_tokenizer, de_tokenizer, dev_X[:lim], dev_Y[:lim], max_length)
-    test_X, test_Y = spacy_tokenize(en_tokenizer, de_tokenizer, test_X[:lim], test_Y[:lim], max_length)
+    train_X, train_Y = spacy_tokenize(en_tokenizer, de_tokenizer,
+                                      train_X[:lim],
+                                      train_Y[:lim], MAX_LENGTH)
+    dev_X, dev_Y = spacy_tokenize(en_tokenizer,
+                                  de_tokenizer,
+                                  dev_X[:lim], dev_Y[:lim], MAX_LENGTH)
+    test_X, test_Y = spacy_tokenize(en_tokenizer, de_tokenizer,
+                                    test_X[:lim], test_Y[:lim], MAX_LENGTH)
     train_Y = add_eos_bos(train_Y)
     # link_vectors_to_models(nlp_en.vocab)
     # link_vectors_to_models(nlp_de.vocab)
@@ -192,37 +196,34 @@ def main(nH=6, dropout=0.1, nS=6, nB=15, nE=20, use_gpu=-1, lim=2000):
     en_embeddings = Embed(MODEL_SIZE, nM=MODEL_SIZE, nV=nTGT)
     de_embeddings = Embed(MODEL_SIZE, nM=MODEL_SIZE, nV=nTGT)
     model = EncoderDecoder(nTGT=nTGT)
+
+
+
+    def track_progress():
+        b_dev, y = get_model_inputs(100, dev_X[:100], dev_Y[:100], en_word2indx,
+                                        de_word2indx, en_embeddings,
+                                        de_embeddings, X_positions, dataset=True)
+        scores = model(b_dev)
+        categorical_y = to_categorical(y, nb_classes=scores.shape[2])
+        scores = scores.reshape(categorical_y.shape)
+        if len(scores.shape) == 1:
+            correct = ((scores >= 0.5) == (categorical_y >= 0.5)).sum()
+        else:
+            correct = (scores.argmax(axis=1) == categorical_y.argmax(axis=1)).sum()
+        print(correct / categorical_y.shape[0])
+
+
     with model.begin_training(train_X, train_Y, batch_size=nB, nb_epoch=nE) \
             as (trainer, optimizer):
         trainer.dropout = dropout
         trainer.dropout_decay = 1e-4
-
-        ''' add beginning and ending of sentence marks '''
+        trainer.each_epoch.append(track_progress)
         for pairs, pad_masks, lengths in trainer.batch_mask(train_X, train_Y):
-            X_text, y_text = pairs
-            X_mask, y_mask = pad_masks
-            nL = X_mask.shape[1]
-            nX, nY = lengths
-            ''' numericalize text '''
-            X_num = Model.ops.xp.empty([1, nB, nL], dtype=Model.ops.xp.int)
-            y_num = Model.ops.xp.empty([1, nB, nL], dtype=Model.ops.xp.int)
-            for i, _x in enumerate(X_text):
-                X_num[0][i][:] = numericalize(en_word2indx, _x)
-            for i, _y in enumerate(y_text):
-                y_num[0][i][:] = numericalize(de_word2indx, _y)
-            ''' get text embeddings '''
-            X_emb0, backprop_X_emb0 = en_embeddings.begin_update(X_num)
-            y_emb0, backprop_y_emb0 = de_embeddings.begin_update(y_num)
-
-            ''' Text embeddings must be reshaped '''
-            X_emb1 = X_emb0.reshape(nB, nL, MODEL_SIZE)
-            y_emb1 = y_emb0.reshape(nB, nL, MODEL_SIZE)
-            ''' add position encodings '''
-            X = X_emb1 + X_positions[:nL]
-            y = y_emb1 + X_positions[:nL]
-            X = X.astype(Model.ops.xp.float32)
-            y = y.astype(Model.ops.xp.float32)
-            b0 = Batch((X, y), (X_mask, y_mask), (nX, nY))
+            nL = pad_masks[0].shape[1]
+            b0, X, y, backprop_X_emb0, backprop_y_emb0, y_num = \
+                get_model_inputs(nB, pairs, pad_masks, lengths, en_word2indx,
+                                 de_word2indx, en_embeddings,
+                                 de_embeddings, X_positions)
             yh, backprop = model.begin_update(b0, drop=trainer.dropout)
             yh2d = yh.reshape(-1, nTGT)
             y_num2d = y_num.reshape(-1)
@@ -236,8 +237,6 @@ def main(nH=6, dropout=0.1, nS=6, nB=15, nE=20, use_gpu=-1, lim=2000):
             backprop_X_emb0(dX_emb0, optimizer)
             backprop_y_emb0(dy_emb0, optimizer)
 
-        with model.use_params(optimizer.averages):
-            print("End: %.3f" % model.evaluate(train_X, train_Y))
 
 if __name__ == '__main__':
     plac.call(main)
