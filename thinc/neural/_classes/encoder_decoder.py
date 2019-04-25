ee377461c9246e56df1e2804a74b2993000f0580
Giannis Daras <daras.giannhs@gmail.com>
2019-04-25 16:10:10 +0300

    Remove deprecated code

--- thinc/neural/_classes/encoder_decoder.py
+++ thinc/neural/_classes/encoder_decoder.py
@@ -9,6 +9,7 @@ from .maxout import Maxout
 from .resnet import Residual
 from .affine import Affine
 from .multiheaded_attention import MultiHeadedAttention
+from .positionwise_ffd import PositionwiseFeedForward
 import copy
 import math
 import numpy as np
@@ -147,22 +148,3 @@ class DecoderLayer(Model):
             dX0 += dX
             return (dY0, dX0)
         return (Y7, X0, X_mask, Y_mask), finish_update
-
-
-class PositionwiseFeedForward(Model):
-    def __init__(self, nM=300, nO=300, dropout=0.0):
-        Model.__init__(self)
-        self.ffd1 = with_reshape(ReLu(nM, nO))
-        self.ffd2 = with_reshape(Affine(nO, nM))
-        self.layers_ = [self.ffd1, self.ffd2]
-        self.nO = nO
-
-    def begin_update(self, X0, drop=0.0):
-        X1, b_ffd1 = self.ffd1.begin_update(X0)
-        X2, b_ffd2 = self.ffd2.begin_update(X1)
-
-        def finish_update(dX2, sgd=None):
-            dX1 = b_ffd2(dX2, sgd=sgd)
-            dX0 = b_ffd1(dX1, sgd=sgd)
-            return dX0
-        return X2, finish_update
