47b69c75d780c23ac678f8135ba3a49295aeb28f
Giannis Daras <daras.giannhs@gmail.com>
2019-04-30 18:42:17 +0300

    Change model architecture

--- thinc/neural/_classes/encoder_categorizer.py
+++ thinc/neural/_classes/encoder_categorizer.py
@@ -18,7 +18,8 @@ class Categorizer(Model):
         self.nO = nO
         self.nS = nS
         self.enc = clone(EncoderLayer(nM=nM, nH=nH, device=device), nS)
-        self.softmax = with_reshape(Softmax(nI=nM, nO=nO))
+        self.affine = Affine(nI=nM, nO=nM)
+        self.softmax = Softmax(nI=nM, nO=nO)
         self.norm = PyTorchWrapper(PytorchLayerNorm(nM=nM, device=device))
         self.slicer = PyTorchWrapper(PytorchSlicer())
         self.device = device
@@ -28,16 +29,18 @@ class Categorizer(Model):
         X0, Xmask = inputs
         (X1, _,), b_X1 = self.enc.begin_update((X0, Xmask))
         X2, b_X2 = self.norm.begin_update(X1)
-        X3, b_X3 = self.softmax.begin_update(X2)
-        X4, b_X4 = self.slicer.begin_update(X3)
+        X3, b_X3 = self.slicer.begin_update(X2)
+        X4, b_X4 = self.affine.begin_update(X3)
+        X5, b_X5 = self.softmax.begin_update(X4)
 
-        def finish_update(dX4, sgd=None):
-            dX3 = b_X4(dX4)
+        def finish_update(dX5, sgd=None):
+            dX4 = b_X5(dX5, sgd=sgd)
+            dX3 = b_X4(dX4, sgd=sgd)
             dX2 = b_X3(dX3, sgd=sgd)
             dX1 = b_X2(dX2, sgd=sgd)
             dX0 = b_X1(dX1, sgd=sgd)
             return dX0
-        return X4, finish_update
+        return X5, finish_update
 
 
 class PytorchSlicer(nn.Module):
