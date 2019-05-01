ee12de7e70890c9d174758fb5340ca4cc314bfcf
Giannis Daras <daras.giannhs@gmail.com>
2019-05-01 15:18:24 +0300

    Cleanup imports

--- examples/attention_is_all_you_need.py
+++ examples/attention_is_all_you_need.py
@@ -9,7 +9,6 @@ import spacy
 from spacy.tokens import Doc
 from spacy.attrs import ID, ORTH, SHAPE, PREFIX, SUFFIX
 from thinc.extra.datasets import get_iwslt
-from thinc.loss import categorical_crossentropy
 from thinc.neural.util import to_categorical, minibatch
 from thinc.neural._classes.encoder_decoder import EncoderDecoder
 from thinc.i2v import HashEmbed
